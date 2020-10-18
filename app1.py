950509b171dbe5f0d5127c9ea0fe31f26e0b3b76
Tanmay2710 <71245259+Tanmay2710@users.noreply.github.com>
2020-10-18 23:53:55 +0530

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+ 
+df = pd.read_csv('twitter_samples')
+x = df.iloc[:,0].values # Review column as input
+y = df.iloc[:,1].values # Sentiment column as output
+st.title("Sentiment Analysis")
+st.subheader('TFIFD Vectorizer')
+st.write('')
+ 
+text_model = Pipeline([('tfidf',TfidfVectorizer()),('model',MultinomialNB())])
+text_model.fit(x,y)
+message = st.text_area("Enter Text","Type Here ..")
+op = text_model.predict([message])
+if st.button("Predict"):
+  st.title(op)
